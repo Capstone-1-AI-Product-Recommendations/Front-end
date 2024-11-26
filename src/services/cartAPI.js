27d0153109af5050e87c6f55f71c2e12c9308b64
TranThiMyDuyenn <tran.miduyen03@gmail.com>
2024-11-27 00:28:40 +0700

    add mới nhất cart, Productdetail, ForgotPassword

--- src/services/cartAPI.js
+++ src/services/cartAPI.js
@@ -1,22 +1,20 @@
-// src/services/cartAPI.js
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Đảm bảo đây là đúng URL của backend
+  baseURL: 'http://127.0.0.1:8000/api',
 });
 
-// API để lấy giỏ hàng
-export const fetchCart = () => API.get('/cart');
+// API lấy toàn bộ giỏ hàng
+export const getCart = () => API.get('/cart/');
 
-// API để thêm sản phẩm vào giỏ hàng
-export const addToCart = (product) => API.post('/cart', { product });
+// API thêm sản phẩm vào giỏ hàng
+export const addToCart = (productData) => API.post('/cart/', productData);
 
-// API để cập nhật số lượng sản phẩm trong giỏ hàng
-export const updateQuantity = (productId, quantity) =>
-  API.put(`/cart/${productId}`, { quantity });
+// API cập nhật số lượng sản phẩm
+export const updateQuantity = (productId, quantity) => API.put(`/cart/${productId}/`, { quantity });
 
-// API để xóa sản phẩm khỏi giỏ hàng
-export const removeFromCart = (productId) => API.delete(`/cart/${productId}`);
+// API xóa sản phẩm khỏi giỏ hàng
+export const removeFromCart = (productId) => API.delete(`/cart/${productId}/`);
 
-// API để xóa tất cả sản phẩm khỏi giỏ hàng
-export const removeAllItems = () => API.delete('/cart');
+// API xóa toàn bộ giỏ hàng
+export const clearCart = () => API.delete('/cart/');
